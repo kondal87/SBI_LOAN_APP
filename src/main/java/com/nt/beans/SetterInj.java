c889c242aa9753cc4ffa5422af73fa54ce70c203
kondal <tilluyadav@gmail.com>
2025-06-18 16:35:06 +0530

    first pro

---
+++ src/main/java/com/nt/beans/SetterInj.java
@@ -0,0 +1,14 @@
+package com.nt.beans;
+
+public class SetterInj 
+{
+ public SetterInj()
+ {
+	 System.out.println("hii tillu");
+ }
+ 
+ public void setInj(String s)
+ {
+	 System.out.println(s);
+ }
+}
